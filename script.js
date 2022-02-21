b88b26c1b74bd8d2cede96f8964649a792ae9c3b
Roman <snapko0063@gmail.com>
2022-02-21 13:19:36 +0300

    all done

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 function add() {
-    let li = document.createElement('li');
-    li.className = "task";
-    li.innerHTML = document.getElementById('name').value;
-    return list.append(li);
+    let item = document.createElement('div');
+    item.className = "task";
+    item.innerHTML = document.getElementById('name').value;
+    document.getElementById('name').value = "";
+    item.onclick = lineThrough;
+    form.after(item);
+    let delBtn = document.createElement('img');
+    delBtn.src = '/icons/can.png';
+    delBtn.className = 'can';
+    delBtn.onclick = del;
+    item.append(delBtn);
+    return false;
 }
 
 submit.onclick = add;
+
+
+function lineThrough() {
+    if (this.style.textDecoration == 'line-through') {
+        this.style.textDecoration = 'none';
+    } else {
+        this.style.textDecoration = 'line-through';
+    }
+
+}
+
+function del() {
+    this.parentNode.remove()
+}
